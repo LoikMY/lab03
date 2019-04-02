1ca538722b8640b155069c89c27cc56bb799d952
LoikMY <melka1nya@mail.ru>
2019-04-02 14:12:55 +0300

    без изменений

--- main.cpp
+++ main.cpp
@@ -3,87 +3,75 @@
 
 using namespace std;
 
-int main(){
-    size_t numer_count=0;
-    cerr << "Enter number count" << endl;
-    cin >> numer_count;
+int main() {
+    // Ввод данных
+    size_t number_count;
+    cerr << "Enter number count: ";
+    cin >> number_count;
 
-    vector <double> numbers;
-    numbers.resize(numer_count);
-
-    cerr << "Enter numbers" << endl;
-    int i;
-    for (i=0; i < numer_count; i++)
-    {
+    cerr << "Enter numbers: ";
+    vector<double> numbers(number_count);
+    for (size_t i = 0; i < number_count; i++) {
         cin >> numbers[i];
     }
-    size_t bin_count=0;
-    cerr << "Enter bin count" << endl;
+
+    size_t bin_count;
+    cerr << "Enter column count: ";
     cin >> bin_count;
 
-    double max=numbers[0],min=numbers[0];
-    for (i=0; i < numer_count; i++)
-    {
-       if (numbers[i] > max)
-       {
-           max=numbers[i];
-       }
-       else if (numbers[i]< min)
-           min=numbers[i];
+    // Обработка данных
+    double min = numbers[0];
+    double max = numbers[0];
+    for (double number : numbers) {
+        if (number < min) {
+            min = number;
+        }
+        if (number > max) {
+            max = number;
+        }
     }
 
-    vector <size_t> bins(bin_count, 0);
-    for (double number: numbers)
-    {
-        size_t bin_index=0;
-        bin_index = ((number - min) * bin_count )/ (max-min);
-        if (bin_index == bin_count)
-        {
-            bin_index--;
+    vector<size_t> bins(bin_count);
+    for (double number : numbers) {
+        size_t bin = (size_t)((number - min) / (max - min) * bin_count);
+        if (bin == bin_count) {
+            bin--;
+        }
+        bins[bin]++;
+    }
+
+    // Вывод данных
+    const size_t SCREEN_WIDTH = 80;
+    const size_t MAX_ASTERISK = SCREEN_WIDTH - 4 - 1;
+
+    size_t max_count = 0;
+    for (size_t count : bins) {
+        if (count > max_count) {
+            max_count = count;
+        }
+    }
+    const bool scaling_needed = max_count > MAX_ASTERISK;
+
+    for (size_t bin : bins) {
+        if (bin < 100) {
+            cout << ' ';
+        }
+        if (bin < 10) {
+            cout << ' ';
+        }
+        cout << bin << "|";
+
+        size_t height = bin;
+        if (scaling_needed) {
+            const double scaling_factor = (double)MAX_ASTERISK / max_count;
+            height = (size_t)(bin * scaling_factor);
         }
 
-            bins[bin_index]++;
-
+        for (size_t i = 0; i < height; i++) {
+            cout << '*';
+        }
+        cout << '\n';
     }
-	vector <size_t> bins_new(bin_count, 0);
-	for (i = 0; i < bin_count; i++)
-	{
-	size_t	height = 0;
-		for (int j = 0; j <= i; j++)
-		{
-			height = height + bins[j];
-		}
-		bins_new[i] = height;
-	}
-
-    const size_t MAX=80-4;
-    size_t bin_max=bins[0];
-    for (i=0; i<bin_count; i++ )
-        if (bins[i] > bin_max)
-        bin_max = bins[i];
-
- double factor = 1.0;
- if (bin_max > MAX)
- {
-     factor = static_cast <double> (MAX)/ bin_max;
- }
-
-for (size_t bin:bins)
-{
-    size_t height = bin * factor;
-    if (bin < 10)
-    cout << " ";
-    if (bin < 100 )
-    cout << " ";
-
-    cout << bin << '|';
-
-    for (i=0; i< height; i++)
-    {
-        cout << "*";
-    }
-    cout << '\n';
-}
 
     return 0;
 }
